7841e53b0ebd5ab54ff042946b5bcae116d7810e
Manuela <mherreral@eafit.edu.co>
2022-01-28 09:37:14 -0500

    v1 first homework

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Store username</title>
+</head>
+
+<body>
+    <center>
+        <h1>Storing Form data in Database</h1>
+
+        <form action="insert.php" method="post">
+
+
+            <p>
+                <label for="Username">Username:</label>
+                <input type="text" name="username" id="Username">
+            </p>
+
+            <input type="submit" value="Submit">
+        </form>
+    </center>
+</body>
+
+</html>
---
+++ insert.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Insert Page page</title>
+</head>
+
+<body>
+	<center>
+		<?php
+        $servername = "localhost";
+        $user = "root";
+        $passwd = "";
+        $db_name= "homework1"; 
+        
+		$conn = mysqli_connect($servername, $user, $passwd, $db_name);
+		
+		// Check connection
+		if($conn === false){
+			die("ERROR: Could not connect. "
+				. mysqli_connect_error());
+		}
+		
+		// Taking all 5 values from the form data(input)
+		$username = $_REQUEST['username'];
+		
+		// Performing insert query execution
+		// here our table name is college
+		$query = "INSERT INTO Persons VALUES ('$username')";
+		
+		if(mysqli_query($conn, $query)){
+			echo "<h3>data stored in a database successfully."
+				. " Please browse your localhost php my admin"
+				. " to view the updated data</h3>";
+
+			echo nl2br("\n$username\n");
+		} else{
+			echo "ERROR: Hush! Sorry $query. "
+				. mysqli_error($conn);
+		}
+		
+		// Close connection
+		mysqli_close($conn);
+		?>
+	</center>
+</body>
+
+</html>
